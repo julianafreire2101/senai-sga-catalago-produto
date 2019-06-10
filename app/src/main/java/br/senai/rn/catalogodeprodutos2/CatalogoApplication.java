0221618517e69c4b1c9b7e7fd59f463ed89d3ab6
Aluno <aluno@fiern.local>
2019-06-10 17:21:00 -0300

    commit inicial

---
+++ app/src/main/java/br/senai/rn/catalogodeprodutos2/CatalogoApplication.java
@@ -0,0 +1,22 @@
+package br.senai.rn.catalogodeprodutos2;
+
+import android.app.Application;
+
+import br.senai.rn.catalogodeprodutos2.dao.ProdutoDAO;
+import br.senai.rn.catalogodeprodutos2.model.Produto;
+
+public class CatalogoApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CriarProdutosDeTeste();
+    }
+
+    private void CriarProdutosDeTeste() {
+        ProdutoDAO dao = new ProdutoDAO();
+
+        dao.salvar(new Produto("Chocolate", "11223344", "Iel"));
+    }
+}
